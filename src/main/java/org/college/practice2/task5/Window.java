dc4319a0a4d6700b0489984810cd81ccb4aecc86
Jabbss <zhoramc155@gmail.com>
2025-01-21 09:11:12 +0200

    Chain of Responsibility_1

---
+++ src/main/java/org/college/practice2/task5/Window.java
@@ -0,0 +1,22 @@
+package org.college.practice2.task5;
+
+public class Window extends WindowComponent {
+    private String title;
+    private int width;
+    private int height;
+
+    public Window(String name, String title, int width, int height) {
+        this.name = name;
+        this.title = title;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Window: " + title + " [" + width + "x" + height + "]");
+        for (WindowComponent component : nestedComponents.values()) {
+            component.draw();
+        }
+    }
+}
---
+++ src/main/java/org/college/practice2/task5/WindowComponent.java
@@ -0,0 +1,19 @@
+package org.college.practice2.task5;
+
+import java.util.HashMap;
+
+public abstract class WindowComponent {
+    protected String name;
+    protected HashMap<String, WindowComponent> nestedComponents = new HashMap<>();
+
+    public void draw() {
+    }
+
+    public void addComponent(WindowComponent component) {
+        nestedComponents.put(component.name, component);
+    }
+
+    public void removeComponent(String name) {
+        nestedComponents.remove(name);
+    }
+}
